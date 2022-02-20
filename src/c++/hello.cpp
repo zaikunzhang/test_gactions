28e0010dff87220383e6e383d46523f8b6eb2883
Zaikun <zaikunzhang@gmail.com>
2022-02-20 19:38:33 +0800

    test Intel oneAPI

---
+++ src/c++/hello.cpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2020 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello, world!" << endl;
+  return 0;
+}
---
+++ src/fortran/hello.f90
@@ -0,0 +1,6 @@
+! SPDX-FileCopyrightText: 2020 Intel Corporation
+!
+! SPDX-License-Identifier: MIT
+
+print *, "Hello, world!"
+end
