83b14e11f2af0774d87b50a402d9450c364ab8ec
Zaikun <zaikunzhang@gmail.com>
2022-02-21 04:02:24 +0800

    test mex fortran on Windows

--- scripts/intelvars_windows.bat
+++ scripts/intelvars_windows.bat
@@ -5,7 +5,7 @@ for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\
 echo %LATEST_VERSION%
 where ifort.exe
 
-echo ONEAPI_ROOT=C:\Program Files (x86)\Intel\oneAPI\  >> %GITHUB_ENV%
-echo ONEAPI_VER=%LATEST_VERSION%  >> %GITHUB_ENV%
-echo IFORT_COMPILER21=C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\ >> %GITHUB_ENV%
+echo ONEAPI_ROOT=C:\Program Files (x86)\Intel\oneAPI>> %GITHUB_ENV%
+echo ONEAPI_VER=%LATEST_VERSION%>> %GITHUB_ENV%
+echo IFORT_COMPILER21=C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows>> %GITHUB_ENV%
 mklink "C:\Windows\ifort.exe" "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\bin\intel64\ifort.exe"
