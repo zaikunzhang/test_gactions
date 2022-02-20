3978e393bfe5a416104d3f2c829c9ec0d0f40755
Zaikun <zaikunzhang@gmail.com>
2022-02-21 03:57:13 +0800

    test mex fortran on Windows

--- scripts/intelvars_windows.bat
+++ scripts/intelvars_windows.bat
@@ -1,24 +1,11 @@
-REM SPDX-FileCopyrightText: 2020 Intel Corporation
-REM
-REM SPDX-License-Identifier: MIT
 
-set LANGUAGE=%1
-set VS_VER=%2
-
-IF "%VS_VER%"=="2017_build_tools" (
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-)
-
-IF "%VS_VER%"=="2019_build_tools" (
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-)
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
 @call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
 
 echo %LATEST_VERSION%
-echo "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
 where ifort.exe
 
-mklink "C:\Windows\ifort.exe" "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\bin\intel64\ifort.exe"
 echo ONEAPI_ROOT=C:\Program Files (x86)\Intel\oneAPI\  >> %GITHUB_ENV%
-echo FORT_COMPILER21=C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\ >> %GITHUB_ENV%
+echo ONEAPI_VER=%LATEST_VERSION%  >> %GITHUB_ENV%
+echo IFORT_COMPILER21=C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\ >> %GITHUB_ENV%
+mklink "C:\Windows\ifort.exe" "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\bin\intel64\ifort.exe"
