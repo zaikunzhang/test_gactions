381826f456a7bdea2bf863e5469fcc3aa07c0ba1
Zaikun <zaikunzhang@gmail.com>
2022-02-21 11:52:32 +0800

    test ifx

--- scripts/intelvars_windows.bat
+++ scripts/intelvars_windows.bat
@@ -4,9 +4,11 @@ for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\
 
 echo %LATEST_VERSION%
 where ifort.exe
+where ifx.exe
 
 echo ONEAPI_ROOT=C:\Program Files (x86)\Intel\oneAPI>> %GITHUB_ENV%
 echo ONEAPI_VER=%LATEST_VERSION%>> %GITHUB_ENV%
 echo PATH=%PATH%;%ONEAPI_ROOT%\compiler\%LATEST_VERSION%\windows\bin\intel64>> %GITHUB_ENV%
 echo IFORT_COMPILER21=C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows>> %GITHUB_ENV%
 mklink "C:\Windows\ifort.exe" "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\bin\intel64\ifort.exe"
+mklink "C:\Windows\ifx.exe" "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\bin\intel64\ifx.exe"
