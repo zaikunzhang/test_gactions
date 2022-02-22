bef1f6dcecd8e93ed22d3883357f61adc299de35
Zaikun <zaikunzhang@gmail.com>
2022-02-22 14:10:02 +0800

    test msnuild

--- scripts/intelvars_windows.bat
+++ scripts/intelvars_windows.bat
@@ -1,4 +1,6 @@
 mklink /D "C:\Program Files (x86)\Microsoft Visual Studio\2019" "C:\Program Files\Microsoft Visual Studio\2022"
+mklink /D "C:\Program Files (x86)\Microsoft Visual Studio\2020" "C:\Program Files\Microsoft Visual Studio\2022"
+mklink /D "C:\Program Files (x86)\Microsoft Visual Studio\2021" "C:\Program Files\Microsoft Visual Studio\2022"
 
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
 @call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
@@ -7,9 +9,10 @@ echo %LATEST_VERSION%
 where ifort.exe
 where ifx.exe
 
-echo ONEAPI_ROOT=C:\Program Files (x86)\Intel\oneAPI>> %GITHUB_ENV%
-echo ONEAPI_VER=%LATEST_VERSION%>> %GITHUB_ENV%
-echo PATH=%PATH%;%ONEAPI_ROOT%\compiler\%LATEST_VERSION%\windows\bin\intel64;%ONEAPI_ROOT%\compiler\%LATEST_VERSION%\windows\bin>> %GITHUB_ENV%
-echo IFORT_COMPILER21=C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows>> %GITHUB_ENV%
-mklink "C:\Windows\ifort.exe" "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\bin\intel64\ifort.exe"
-mklink "C:\Windows\ifx.exe" "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\bin\ifx.exe"
+setx "ONEAPI_ROOT" "C:\Program Files (x86)\Intel\oneAPI" /M
+echo "ONEAPI_ROOT=%ONEAPI_ROOT%" >> %GITHUB_ENV%
+echo "ONEAPI_VER=%LATEST_VERSION%" >> %GITHUB_ENV%
+echo "PATH=%PATH%;%ONEAPI_ROOT%\compiler\%LATEST_VERSION%\windows\bin\intel64;%ONEAPI_ROOT%\compiler\%LATEST_VERSION%\windows\bin" >> %GITHUB_ENV%
+echo "IFORT_COMPILER21=%ONEAPI_ROOT%\compiler\%LATEST_VERSION%\windows" >> %GITHUB_ENV%
+mklink "C:\Windows\ifort.exe" "%ONEAPI_ROOT%\compiler\%LATEST_VERSION%\windows\bin\intel64\ifort.exe"
+mklink "C:\Windows\ifx.exe" "%ONEAPI_ROOT%\compiler\%LATEST_VERSION%\windows\bin\ifx.exe"
