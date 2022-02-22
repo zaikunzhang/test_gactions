f7fddd3cf43d79410f8af38a92a16f75629028b1
Zaikun <zaikunzhang@gmail.com>
2022-02-22 13:54:41 +0800

    test link

--- scripts/intelvars_windows.bat
+++ scripts/intelvars_windows.bat
@@ -1,3 +1,4 @@
+mklink /D "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise" "C:\Program Files\Microsoft Visual Studio\2022\Enterprise"
 
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
 @call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
