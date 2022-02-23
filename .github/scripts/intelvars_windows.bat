6b0f39f62eb0f2aaf24d7becc90065cdbbb7aa87
Zaikun <zaikunzhang@gmail.com>
2022-02-23 20:54:18 +0800

    test mex on windows

---
+++ .github/scripts/intelvars_windows.bat
@@ -0,0 +1,14 @@
+for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
+@call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
+
+echo %LATEST_VERSION%
+where ifort.exe
+where ifx.exe
+
+echo "ONEAPI_ROOT=C:\Program Files (x86)\Intel\oneAPI" >> %GITHUB_ENV%
+echo "ONEAPI_VER=%LATEST_VERSION%" >> %GITHUB_ENV%
+echo "PATH=%PATH%;C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\bin\intel64;C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\bin" >> %GITHUB_ENV%
+echo "IFORT_COMPILER21=C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows" >> %GITHUB_ENV%
+mklink /D "C:\Program Files (x86)\Intel\oneAPI\compiler\latest" "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%"
+mklink "C:\Windows\ifort.exe" "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\bin\intel64\ifort.exe"
+mklink "C:\Windows\ifx.exe" "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\windows\bin\ifx.exe"
